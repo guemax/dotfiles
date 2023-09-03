08799c386a3ee34274f62e8301126b758b425d9e
guemax <code-mg@mailbox.org>
2023-09-03 19:11:45 +0200

    Refactor xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,35 +5,81 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.Spacing
 import XMonad.Layout.NoBorders
 
-myConfig = def
-  { modMask = myModMask
-  , terminal = myTerminal
-  , borderWidth = myBorderWidth
-  , normalBorderColor  = myNormalBorderColor
-  , focusedBorderColor = myFocusedBorderColor
-  , layoutHook = myLayoutHook
-  }
-  `additionalKeysP`
-  -- Miscellaneous
-  [ ("M-q", restart "xmonad" True)
+
+----------------------------------------------------------------------
+--                                                                  --
+--                      --- Miscellaneous ---                       --
+--                                                                  --
+----------------------------------------------------------------------
+
+myModMask = mod4Mask -- Use Super instead of Alt.
+myTerminal = "alacritty"
+
+
+----------------------------------------------------------------------
+--                                                                  --
+--                         --- Borders ---                          --
+--                                                                  --
+----------------------------------------------------------------------
+
+myBorderWidth = 5
+myNormalBorderColor = "#52494e"
+myFocusedBorderColor = "#c73c3f"
+
+
+----------------------------------------------------------------------
+--                                                                  --
+--                          --- Layout ---                          --
+--                                                                  --
+----------------------------------------------------------------------
+
+myLayoutHook = avoidStruts
+               $ smartSpacingWithEdge 10
+               $ smartBorders
+               $ layoutTall
+               ||| layoutMirror
+               ||| layoutFull
+  where
+    layoutTall = Tall 1 (3/100) (1/2)
+    layoutMirror = Mirror (Tall 1 (3/100) (3/5))
+    layoutFull = Full
+
+
+----------------------------------------------------------------------
+--                                                                  --
+--                       --- Keybindings ---                        --
+--                                                                  --
+----------------------------------------------------------------------
+
+myAdditionalKeysP =
+  [ -- Miscellaneous
+    ("M-q", restart "xmonad" True)
   , ("M-f", spawn "firefox")
-  -- Audio
+    -- Audio
   , ("<XF86AudioRaiseVolume>",  spawn "amixer set Master 5%+")
   , ("<XF86AudioLowerVolume>",  spawn "amixer set Master 5%-")
   , ("<XF86AudioMute>",         spawn "amixer set Master toggle")
-  -- Monitor Brightness
+    -- Monitor Brightness
   , ("<XF86MonBrightnessUp>",   spawn "brightnessctl set +10%")
   , ("<XF86MonBrightnessDown>", spawn "brightnessctl set 10%-")
   ]
 
-myModMask = mod4Mask -- Use Super instead of Alt
-myTerminal = "alacritty"
 
-myBorderWidth = 5
-myNormalBorderColor = "#303540"
-myFocusedBorderColor = "#565f73"
+----------------------------------------------------------------------
+--                                                                  --
+--                      --- Main Function ---                       --
+--                                                                  --
+----------------------------------------------------------------------
 
--- myLayoutHook = spacingRaw False (Border 10 0 10 0) True (Border 0 10 0 10) True $ Tall 1 (3/100) (1/2) ||| Full
-myLayoutHook = smartSpacingWithEdge 10 $ smartBorders $ Tall 1 (3/100) (1/2) ||| Full
-
-main = xmonad $ ewmhFullscreen $ ewmh $ myConfig
+main = xmonad
+       . ewmhFullscreen
+       . ewmh
+       $ def
+       { modMask = myModMask
+       , terminal = myTerminal
+       , borderWidth = myBorderWidth
+       , normalBorderColor  = myNormalBorderColor
+       , focusedBorderColor = myFocusedBorderColor
+       , layoutHook = myLayoutHook
+       }
+       `additionalKeysP` myAdditionalKeysP
