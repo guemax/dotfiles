898c3152b48a125f729fb25b5395b2f592fd9126
guemax <code-mg@mailbox.org>
2023-09-03 13:33:41 +0200

    Add move-text package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -92,6 +92,22 @@
   :config
   (global-set-key (kbd "<escape>") #'god-mode-all))
 
+(use-package move-text
+  :ensure t
+  :bind (("M-p" . move-text-up)
+	 ("M-n" . move-text-down))
+  :config
+  (advice-add 'move-text-up :after 'indent-region-advice)
+  (advice-add 'move-text-down :after 'indent-region-advice))
+
+(defun indent-region-advice (&rest ignored)
+  (let ((deactivate deactivate-mark))
+    (if (region-active-p)
+        (indent-region (region-beginning) (region-end))
+      (indent-region (line-beginning-position) (line-end-position)))
+    (setq deactivate-mark deactivate)))
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;                                                                ;;;
 ;;;                       --- Completion ---                       ;;;
