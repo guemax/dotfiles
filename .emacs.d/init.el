1474f6fc64f7d3f4b3778db81663d1245a543913
guemax <code-mg@mailbox.org>
2023-09-12 19:32:23 +0200

    Reformat license comments to fit into 70 chars wide buffer

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,22 +2,24 @@
 
 ;; Copyright (c) 2023 guemax
 
-;; Permission is hereby granted, free of charge, to any person obtaining a copy
-;; of this software and associated documentation files (the "Software"), to deal
-;; in the Software without restriction, including without limitation the rights
-;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-;; copies of the Software, and to permit persons to whom the Software is
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use, copy,
+;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;; of the Software, and to permit persons to whom the Software is
 ;; furnished to do so, subject to the following conditions:
 
-;; The above copyright notice and this permission notice shall be included in all
-;; copies or substantial portions of the Software.
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
 
-;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ;; SOFTWARE.
 
 
@@ -251,7 +253,10 @@ Exempt major modes are defined in `display-line-numbers-exempt-modes'."
   (setq dashboard-projects-backend 'projectile)
   (setq dashboard-items '((recents . 3)
 			  (projects . 3)
-			  (agenda . 3)))
+			  (agenda . 5)))
+  (setq dashboard-center-content t)
+  (setq dashboard-banner-logo-title "Welcome, my friend!")
+  (setq dashboard-week-agenda t)
   (setq dashboard-filter-agenda-entry 'dashboard-no-filter-agenda)
   (setq dashboard-startup-banner 1)
   (setq dashboard-footer-messages
@@ -384,8 +389,8 @@ Exempt major modes are defined in `display-line-numbers-exempt-modes'."
   :ensure t
   :config
   (setq org-hide-emphasis-markers t)
-  (setq org-agenda-files '("~/org/Birthdays.org"
-                           "~/org/Tasks.org"))
+  (setq org-agenda-files '("~/org/Tasks.org"
+			   "~/org/Birthdays.org"))
   (setq org-agenda-start-with-log-mode t)
   (setq org-log-done 'time)
   (setq org-log-into-drawer t)
@@ -404,13 +409,6 @@ Exempt major modes are defined in `display-line-numbers-exempt-modes'."
           ("batch" . ?b)
           ("note" . ?n)
           ("idea" . ?i))))
-
-(use-package org-bullets
-  :ensure t
-  :after org
-  :hook (org-mode . org-bullets-mode)
-  :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
 ;;; Exporting org mode files to markdown.
 (require 'ox-md)
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,26 +2,27 @@
 
 Copyright (c) 2023 guemax
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports = [
@@ -29,6 +30,11 @@ SOFTWARE.
     ./hardware-configuration.nix
     <home-manager/nixos>
   ];
+
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem
+    (lib.getName pkg) [
+      "epson-inkjet-printer-workforce-840-series"
+    ];
 
   
   ######################################################################
@@ -127,6 +133,9 @@ SOFTWARE.
 
   services.xserver.displayManager.defaultSession = "none+xmonad";
 
+  # Touchpad support.
+  services.xserver.libinput.enable = true;
+
 
   ######################################################################
   ###                                                                ###
@@ -185,6 +194,22 @@ SOFTWARE.
   };
   
 
+  ######################################################################
+  ###                                                                ###
+  ###                          --- CUPS ---                          ###
+  ###                                                                ###
+  ######################################################################
+
+  services.printing.enable = true;
+  services.printing.drivers = with pkgs; [
+    epson-escpr
+    # Custom Nix package written by my father, which uses the
+    # proprietary Epson driver for the Workforce 840 to enable double
+    # sided printing as well.
+    (callPackage /etc/nixos/epson-workforce-840.nix {})
+  ];
+
+  
   ######################################################################
   ###                                                                ###
   ###                     --- User Accounts ---                      ###
@@ -257,6 +282,7 @@ SOFTWARE.
     ## OS utilities. ##
     ###################
     thunderbird
+    libreoffice
     borgbackup
     localsend  # Open Source alternative to Air Drop.
     anki
