aecd83fec71eb75ea08e2a132f1b5c3d7a8ee7b8
guemax <code-mg@mailbox.org>
2023-10-01 19:03:56 +0200

    Update rofi config

--- nixos/modules/system-packages.nix
+++ nixos/modules/system-packages.nix
@@ -35,6 +35,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     ###################
     ## OS utilities. ##
     ###################
+    papirus-icon-theme
     phinger-cursors
     lxappearance
     thunderbird
